dd5ff5d763cceec09a2ba1aa0d1e320534e89d66
Evan <ewpw5678@gmail.com>
2021-12-08 23:28:50 -0800

    write getDisplays function

--- functions/index.js
+++ functions/index.js
@@ -1,9 +1,24 @@
-const functions = require("firebase-functions");
+const functions = require('firebase-functions');
+const admin = require('firebase-admin');
+
+admin.initializeApp();
 
 // // Create and Deploy Your First Cloud Functions
 // // https://firebase.google.com/docs/functions/write-firebase-functions
 //
 exports.helloWorld = functions.https.onRequest((request, response) => {
-  functions.logger.info("Hello logs!", {structuredData: true});
-  response.send("Hello, world!");
+  functions.logger.info('Hello logs!', {structuredData: true});
+  response.send('Hello, world!');
+});
+
+exports.getDisplays = functions.https.onRequest((req, res) => {
+  admin.firestore().collection('displays').get()
+    .then(data => {
+      let displays = [];
+      data.forEach(doc => {
+        displays.push(doc.data());
+      });
+      return res.json(displays);
+    })
+    .catch(err => console.error(err));
 });
